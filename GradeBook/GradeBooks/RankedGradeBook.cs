e2d69eff3dd55c1afd3a26d6a05e56185a1a1dfc
Beez-kneez <43068371+Beez-kneez@users.noreply.github.com>
2018-11-16 14:51:11 +0000

    New Grade changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,41 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            else
+            {
+                var dropGrade = (int)Math.Round(Students.Count * 0.2);
+                var aveGrade = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+                if (aveGrade[dropGrade - 1] <= averageGrade)
+                {
+                    return 'A';
+                }
+                else if (aveGrade[dropGrade * 2 - 1] <= averageGrade)
+                {
+                    return 'B';
+                }
+                else if (aveGrade[dropGrade * 3 - 1] <= averageGrade)
+                {
+                    return 'C';
+                }
+                else if (aveGrade[dropGrade * 4 - 1] <= averageGrade)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'f';
+                }
+            }
+        }
+
     }
 }
